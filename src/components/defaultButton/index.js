d80e802958b6c8e5d10770e26b901bd92581c07c
lp-mateus <170110842@aluno.unb.br>
2021-09-18 16:07:15 -0300

    start: Criação da aplicação
    Init  commit de criação do projeto da Plataforma SCC.
    - Full Stack
    - Sistema de Cadastro de Clientes
    - Plataforma Online

---
+++ src/components/defaultButton/index.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { red } from '@mui/material/colors';
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  fontFamily: '"Segoe UI"',
+  borderRadius: '1rem',
+  color: "whitesmoke",
+  backgroundColor: red[300],
+  '&:hover': {
+    backgroundColor: red[700],
+  },
+}));
+
+export default function CustomizedButtons(props) {
+  // Váriavel de estado - Hook useState
+  const [count, setCount] = useState(0);
+
+  // Método complexo que utiliza o setCount (Atualiza o estado)
+  function incrementar(){
+    setCount(count + 1);
+    console.log(count);
+  }
+
+  return (
+    <Stack spacing={2} direction="row">
+      <ColorButton variant="contained" onClick={incrementar}>{props.name} + {count}</ColorButton>
+    </Stack>
+  );
+}
---
+++ src/services/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOYZZZnx-826cactOO-qk0kKdD93kbCNc",
+  authDomain: "backend-mlp-scc.firebaseapp.com",
+  projectId: "backend-mlp-scc",
+  storageBucket: "backend-mlp-scc.appspot.com",
+  messagingSenderId: "287322131820",
+  appId: "1:287322131820:web:92b3e4c6cc0949b1c37749"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
